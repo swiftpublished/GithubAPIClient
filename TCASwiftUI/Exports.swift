932b8fcea05e72eef1361c8403f267873fffb971
Muralidharan Kathiresan <kmuralidharan91@gmail.com>
2023-10-01 14:19:07 +0100

    Adds TCASwiftUI framework for TCA's Version of Github API Client app

---
+++ TCASwiftUI/Exports.swift
@@ -0,0 +1,9 @@
+//
+//  Exports.swift
+//  TCASwiftUI
+//
+//  Created by Muralidharan Kathiresan on 01/10/23.
+//
+
+@_exported import SwiftUI
+@_exported import Core
---
+++ TCASwiftUI/TCASwiftUI.h
@@ -0,0 +1,18 @@
+//
+//  TCASwiftUI.h
+//  TCASwiftUI
+//
+//  Created by Muralidharan Kathiresan on 01/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TCASwiftUI.
+FOUNDATION_EXPORT double TCASwiftUIVersionNumber;
+
+//! Project version string for TCASwiftUI.
+FOUNDATION_EXPORT const unsigned char TCASwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TCASwiftUI/PublicHeader.h>
+
+
