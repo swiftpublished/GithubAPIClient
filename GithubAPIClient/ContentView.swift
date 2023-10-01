9e4ed608fbeb656fbcc8a7a88f1e8558950a1d6b
Muralidharan Kathiresan <kmuralidharan91@gmail.com>
2023-10-01 13:31:52 +0100

    Initial Commit

---
+++ GithubAPIClient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubAPIClient
+//
+//  Created by Muralidharan Kathiresan on 01/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubAPIClient/GithubAPIClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAPIClientApp.swift
+//  GithubAPIClient
+//
+//  Created by Muralidharan Kathiresan on 01/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubAPIClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
