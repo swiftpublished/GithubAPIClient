b0cfa734587270d01d7025bf392e11f919bc04c7
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-03-05 00:15:44 +0530

    RepositoryList

--- TCASwiftUI/RepositoryList/RepositoryListFeature.swift
+++ TCASwiftUI/RepositoryList/RepositoryListFeature.swift
@@ -5,63 +5,66 @@
 //  Created by Muralidharan Kathiresan on 01/10/23.
 //
 
-public struct RepositoryListFeature: Reducer {
-    public struct State: Equatable {
-        public var repos: IdentifiedArrayOf<RepositoryRowFeature.State>
-        public var isLoading: Bool
-        public var errorMessage: String?
-        
+@Reducer
+public struct RepositoryListFeature {
+    @ObservableState
+    public struct State {
+        var rows: IdentifiedArrayOf<RepositoryRowFeature.State>
+        var isLoading: Bool
+        var errorMessage: String?
+
         public init(
-            repos: IdentifiedArrayOf<RepositoryRowFeature.State> = [],
+            rows: IdentifiedArrayOf<RepositoryRowFeature.State> = [],
             isLoading: Bool = true,
             errorMessage: String? = nil
         ) {
-            self.repos = repos
+            self.rows = rows
             self.isLoading = isLoading
         }
     }
-    
-    public enum Action: Equatable {
+
+    public enum Action {
         case fetchRepos
-        case reposResponse(TaskResult<Repositories>)
-        
-        case repo(id: RepositoryRowFeature.State.ID, action: RepositoryRowFeature.Action)
+        case reposResponse(Result<Repositories, Error>)
+
+        case rows(IdentifiedActionOf<RepositoryRowFeature>)
     }
-    
+
     @Dependency(\.repositoryClient) var repositoryClient
-    
+
     public init() {  }
-    
+
     public var body: some ReducerOf<Self> {
         Reduce<State, Action> { state, action in
             switch action {
             case .fetchRepos:
                 state.isLoading = true
-                
+
                 return .run { send in
-                    await send(
-                        .reposResponse(
-                            TaskResult { try await repositoryClient.repos() }
-                        )
-                    )
+                    do {
+                        let repos = try await repositoryClient.repos()
+                        await send(.reposResponse(.success(repos)))
+                    } catch {
+                        await send(.reposResponse(.failure(error)))
+                    }
                 }
-                
+
             case let .reposResponse(.success(repos)):
                 state.isLoading = false
                 let repoStates = repos.map(RepositoryRowFeature.State.init(repo:))
-                state.repos = IdentifiedArray(uniqueElements: repoStates)
+                state.rows = IdentifiedArray(uniqueElements: repoStates)
                 return .none
-                
+
             case let .reposResponse(.failure(error)):
                 state.isLoading = false
                 state.errorMessage = error.localizedDescription
                 return .none
-                
-            case .repo:
+
+            case .rows:
                 return .none
             }
         }
-        .forEach(\.repos, action: /Action.repo(id:action:)) {
+        .forEach(\.rows, action: \.rows) {
             RepositoryRowFeature()
         }
     }
